34fa3bb31d3161c97e56524ce00d0e6301a25fbe
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2024-03-07 09:56:48 -0300

    Delete Algoritmo56.py

--- Algoritmo56.py
+++
@@ -1,9 +0,0 @@
-contador = 0
-Numeros = []
-while contador < 5:
-    contador = contador + 1
-    valor = int(input(f"{contador})Digite o valor a ser guardado:"))
-    Numeros.append(valor)
-
-media = sum(Numeros)/len(Numeros)
-print(f'A media de valores sera:{media}')
