a3c8190d563b52b8c98d1933dabe74743c9464d4
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2024-03-07 09:55:57 -0300

    Delete Algoritmo54.py

--- Algoritmo54.py
+++
@@ -1,4 +0,0 @@
-cotacaoDolar = float(input("Informe a cotação do dolar: "))
-Dolar = float(input("Informe a quantidade de dolar a ser convertida em real:"))
-Real = cotacaoDolar * Dolar
-print(f'O seu dinheiro em reais vale:{Real} R$')
